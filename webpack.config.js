2ec681232e766034cd2900cc3356c9e64402b243
Paul Morelle <madprog@htkc.org>
2017-07-23 17:58:42 +0200

    Add a with-edition compilation mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path');
+const webpack = require('webpack');
+
+const ENABLE_EDITION = [
+  '1', 'yes', 'enable', 'y', 'true'
+].indexOf((process.env.ENABLE_EDITION || '').toLowerCase()) >= 0;
 
 module.exports = {
   context: path.join(__dirname, 'src'),
@@ -27,6 +32,14 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'ENABLE_EDITION': JSON.stringify(ENABLE_EDITION ? true : false),
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+  ],
   resolve: {
     modules: [
       path.join(__dirname, 'node_modules'),
